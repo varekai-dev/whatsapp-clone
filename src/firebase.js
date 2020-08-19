7eb2fc1b13c323894f561a81523cbf082bf262a0
Serhii <serhijsav@gmail.com>
2020-08-19 23:36:20 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAAnZlgT5q2q8rSUFb1Srvc8_kv8I7V7p0",
+  authDomain: "whatsapp-clone-9ce12.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-9ce12.firebaseio.com",
+  projectId: "whatsapp-clone-9ce12",
+  storageBucket: "whatsapp-clone-9ce12.appspot.com",
+  messagingSenderId: "524884924786",
+  appId: "1:524884924786:web:e5e3cd4bba3b839c1a25eb",
+  measurementId: "G-4EWBFSWJ2H",
+}
